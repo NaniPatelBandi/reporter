959d9e92a79e52647b65ca40fbc57d363083bfbd
Nani Patel <nativeandbase@gmail.com>
2022-06-27 20:54:24 +0530

    firebase is set up with react and hosted

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMPgn2y_8KqQ-v9woVatj7BzbeelSLx8g",
+  authDomain: "localnewsapp2022.firebaseapp.com",
+  projectId: "localnewsapp2022",
+  storageBucket: "localnewsapp2022.appspot.com",
+  messagingSenderId: "38526752238",
+  appId: "1:38526752238:web:0c2e5e8455e9bbfa0e6aff",
+  measurementId: "G-MDYDZ8VHYH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
