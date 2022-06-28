fd2fa5a38ad73993d6969a89c7abdea5723d8518
Nani Patel <nativeandbase@gmail.com>
2022-06-28 22:20:00 +0530

    login and logout completed with UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,50 +6,71 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
+// css
+import "../css/login.css";
 const Login = () => {
   console.log("from login page");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user, loading, error] = useAuthState(auth);
+  const [error, setError] = useState("");
+  const [user, loading] = useAuthState(auth);
 
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log("form use effect");
     if (user) navigate("/write");
-  }, [user]);
+  }, [user, navigate]);
 
-  const signIn = async (email, password) => {
+  const signIn = async (e, email, password) => {
+    if (!email || !password) return;
+    e.preventDefault();
     try {
       const reporter = await signInWithEmailAndPassword(auth, email, password);
       console.log(reporter.user);
+      setEmail("");
+      setPassword("");
     } catch (error) {
       console.log(error.message);
+      setError("Email or Password is Incorrect");
+      setPassword("");
     }
   };
   if (loading)
     return (
-      <div>
+      <div className="loadingDiv">
         <p>Loading...</p>
       </div>
     );
   return (
-    <div>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter Your E-mail"
-        required
-        autoFocus
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Enter Your Password"
-        required
-      />
-      <button onClick={() => signIn(email, password)}>LogIn</button>
+    <div className="loginContainer">
+      <div className="error">{error}</div>
+      <form>
+        <input
+          className="loginInput"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter Your E-mail"
+          required
+          autoFocus
+        />
+        <input
+          className="loginInput"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter Your Password"
+          required
+        />
+        <button
+          type="submit"
+          className="loginButton"
+          onClick={(e) => signIn(e, email, password)}
+        >
+          LogIn
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/Write.js
+++ src/components/Write.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 const Write = () => {
+  const [user] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      return navigate("/");
+    }
+  }, [user, navigate]);
+
   const logOut = () => {
     signOut(auth);
   };
+  // if (!user) {
+  //   navigate("/");
+  // }
   return (
     <div>
       <p>this is from write</p>
